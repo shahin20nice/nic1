5dd540c1c7d46c729cde6c921b46713c335b6beb
shahin20nice <shahin.nice20@gmail.com>
2017-04-02 14:33:59 +0430

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -20,7 +20,7 @@ local function plugin_exists( name )
 end
 
 local function list_all_plugins(only_enabled, msg)
-  local tmp = '\n\n@titantims'
+  local tmp = '\n\n@Nice20Team'
   local text = ''
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
@@ -66,7 +66,7 @@ local function list_plugins(only_enabled, msg)
      -- text = text..v..'  '..status..'\n'
     end
   end
-  text = "\n_🔃All Plugins Reloaded_\n\n"..nact.." *✔️Plugins Enabled*\n"..nsum.." *📂Plugins Installed*\n\n@titantims"
+  text = "\n_🔃All Plugins Reloaded_\n\n"..nact.." *✔️Plugins Enabled*\n"..nsum.." *📂Plugins Installed*\n\n@Nice20Team"
   tdcli.sendMessage(msg.to.id, msg.id_, 1, text, 1, 'md')
 end
 
@@ -239,4 +239,4 @@ return {
 
 end
 
---@titantims
+--@Nice20Team
